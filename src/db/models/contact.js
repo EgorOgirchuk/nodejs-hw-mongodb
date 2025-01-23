729770ba3809bef651670e54da2ea80bf5082325
EgorOgichuk <123835482+EgorOgirchuk@users.noreply.github.com>
2025-01-23 12:24:42 +0200

    change isFavourite

--- src/db/models/contact.js
+++ src/db/models/contact.js
@@ -1,37 +1,38 @@
-import { Schema, model } from 'mongoose';
+import {Schema, model} from 'mongoose';
 
 const contactSchema = new Schema(
-  {
-    name: {
-      type: String,
-      required: true,
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        phoneNumber: {
+            type: String,
+            required: true,
+        },
+        email: {
+            type: String,
+            required: false,
+        },
+
+        isFavourite: {
+            type: Boolean,
+            default: false,
+        },
+        contactType: {
+            type: String,
+            required: true,
+            enum: ['work', 'home', 'personal'],
+            default: 'personal',
+        },
     },
-    phoneNumber: {
-      type: String,
-      required: true,
+    {
+        // Це додає до об'єкту два поля:
+        // createdAt(дата створення) та updatedAt(дата оновлення),
+        // і їх не потрібно додавати вручну.
+        timestamps: true,
+        versionKey: false,
     },
-    email: {
-      type: String,
-      required: false,
-    },
-    isFavorite: {
-      type: Boolean,
-      default: false,
-    },
-    contactType: {
-      type: String,
-      required: true,
-      enum: ['work', 'home', 'personal'],
-      default: 'personal',
-    },
-  },
-  {
-    // Це додає до об'єкту два поля:
-    // createdAt(дата створення) та updatedAt(дата оновлення),
-    // і їх не потрібно додавати вручну.
-    timestamps: true,
-    versionKey: false,
-  },
 );
 
 export const ContactsCollection = model('contacts', contactSchema);
